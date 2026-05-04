1ec810b8611793e017796f1c3804f1e04d23a221
Arica McEvoy <arica.mcevoy@gmail.com>
2026-05-04 11:48:46 -0700

    Add continuous loop and rolling average simulation

--- temp.cpp
+++ temp.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <thread>
+#include <chrono>
 
 int  main() {
-	std::srand(std::time(nullptr));
+   std::srand(std::time(nullptr));
 
-	const int sampleCount = 10;
-	float sum = 0.0;
+   const int sampleWindow = 5;
+   float samples[sampleWindow] = {0};
+   int index = 0;
+   int count = 0;
 
-	std::cout << "Temperature Simulation\n";
-	std::cout << "Taking " << sampleCount << " samples...\n\n";
+   while (true) {
+      float tempF = 70.0 + ((std::rand() % 100) - 50) / 10.0;
 
-	for (int i = 1; i <= sampleCount; i++) {
-		float tempF = 70.0 + ((std::rand() % 100) - 50) / 10.0; 
+      samples[index] = tempF;
+      index = (index + 1) % sampleWindow;
 
-		std::cout << "Sample " << i << ": " << tempF << " F\n";
+      if (count < sampleWindow) count++;
+      
+      float sum = 0;
+      for (int i =0; i < count; i++) {
+         sum += samples[i];
+      }
 
-		sum += tempF;
-	}
+      float avg =  sum / count;
 
-	float average = sum / sampleCount;
+      std::cout << "Current: " << tempF << " F | Avg(" << count << "): " << avg << " F\n";
 
-	std::cout << "\nAverage Temp: " << average << " F\n";
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
-	return 0;
+   return 0;
 }
