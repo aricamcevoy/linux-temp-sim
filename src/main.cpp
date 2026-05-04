76789d99bd7c524e336b30401ecd3bc45a647eae
Arica McEvoy <arica.mcevoy@gmail.com>
2026-05-04 13:11:57 -0700

    Refactor temperature simulation into task-style functions

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@
 #include <iomanip>
 #include <filesystem>
 
+float readTemp();
+void updateRollingAverage(float temp, float samples[], int sampleWindow, int &index, int &count, float &avg);
+void displayStatus(float temp, float avg);
+void logTemp(std::ofstream &logFile, float temp, float avg);
+
 int  main() {
    std::srand(std::time(nullptr));
 
@@ -23,34 +28,55 @@ int  main() {
    float samples[sampleWindow] = {0};
    int index = 0;
    int count = 0;
+   float avg = 0.0;
 
    while (true) {
-      float tempF = 70.0 + ((std::rand() % 100) - 50) / 10.0;
+      float temp = readTemp();
 
-      samples[index] = tempF;
-      index = (index + 1) % sampleWindow;
+      updateRollingAverage(temp, samples, sampleWindow, index, count, avg);
+      displayStatus(temp, avg);
+      logTemp(logFile, temp, avg);
 
-      if (count < sampleWindow) count++;
-      
-      float sum = 0;
-      for (int i =0; i < count; i++) {
-         sum += samples[i];
-      }
-
-      float avg =  sum / count;
-
-      auto now = std::time(nullptr);
-
-      logFile << std::put_time(std::localtime(&now), "%Y-%m-%d %H:%M:%S") 
-            << ", " << tempF 
-            << ", " << avg 
-            << "\n";
-
-      logFile.flush();
 
       std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
-   logFile.close();
    return 0;
+}
+
+float readTemp() {
+   float temp = 70.0 + ((std::rand() % 100) - 50) / 10.0;
+
+   return temp;
+}
+
+void updateRollingAverage(float temp, float samples[], int sampleWindow, int &index, int &count, float &avg) {
+   samples[index] = temp;
+   index = (index + 1) % sampleWindow;
+
+   if (count < sampleWindow) count++;
+
+   float sum = 0;
+
+   for (int i =0; i < count; i++) {
+      sum += samples[i];
+   }
+
+   avg =  sum / count;
+}
+
+void displayStatus(float temp, float avg) {
+   std::cout << "Current: " << temp << " F | Avg: " << avg << " F" << std::endl;
+}
+
+void logTemp(std::ofstream &logFile, float temp, float avg) {
+   auto now = std::time(nullptr);
+
+   logFile << std::put_time(std::localtime(&now), "%Y-%m-%d %H:%M:%S")
+         << ", " << temp
+         << ", " << avg
+         << "\n";
+
+   logFile.flush();
+
 }
